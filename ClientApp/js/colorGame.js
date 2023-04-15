7d9a6ea0843979744b2ff682d50f0980d192b2fd
Dani-gr <dani.gr@uma.es>
2023-04-15 02:08:26 +0200

    Study mode of colors complete
    Added hover text for the colors, as well as a button to cycle between two palettes.
    Moreover, changed the nav bar in some menus to be the same (before the React.js migration).

--- ClientApp/js/colorGame.js
+++ ClientApp/js/colorGame.js
@@ -4,25 +4,25 @@ const answerDiv = document.getElementById("answer");
 const colorDisplay = document.getElementById("color-display");
 
 const colors = [
-	"red",
-	"orange",
-	"yellow",
-	"green",
-	"aqua",
-	"blue",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "aqua",
+    "blue",
     "purple"
 ]
 let color;
 let letters;
 
-startGame = function() {
+startGame = function () {
     // Seleccionar el color y las letras
     color = colors[Math.floor(Math.random() * colors.length)];
     letters = color.split("");
     letters.push(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
     letters.push(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
 
-    letters.sort(function() {return Math.random() - 0.5;});
+    letters.sort(function () { return Math.random() - 0.5; });
 
     colorDisplay.style.backgroundColor = color;
     answerDiv.style.backgroundColor = "#fff";
@@ -30,7 +30,7 @@ startGame = function() {
     // Eliminar las letras anteriores
     for (let i = lettersDiv.childElementCount - 1; i >= 0; i--)
         lettersDiv.children[i].remove();
-    
+
     // Agregar letras al contenedor de letras
     for (let i = 0; i < letters.length; i++) {
         const letter = document.createElement("div");
@@ -48,7 +48,7 @@ startGame = function() {
     // Eliminar los espacios anteriores
     for (let i = answerDiv.childElementCount - 1; i >= 0; i--)
         answerDiv.children[i].remove();
-    
+
     // Crear los espacios para la respuesta
     for (let i = 0; i < color.length; i++) {
         const answerSpace = document.createElement("div");
@@ -58,48 +58,48 @@ startGame = function() {
     }
 
     // Hacer que todas las letras sean arrastrables
-    document.querySelectorAll('.letter').forEach(function(value) {
-        value.addEventListener('dragstart', function(e){
+    document.querySelectorAll('.letter').forEach(function (value) {
+        value.addEventListener('dragstart', function (e) {
             e.dataTransfer.setData('text', this.id);
         });
     });
 }
 
 
-document.addEventListener('dragover', function(e) {
-	e.preventDefault();
+document.addEventListener('dragover', function (e) {
+    e.preventDefault();
 });
 
-document.addEventListener('drop', function(e) {
-	e.preventDefault();
-	const data = e.dataTransfer.getData('text');
-	const droppedLetter = document.getElementById(data);
+document.addEventListener('drop', function (e) {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text');
+    const droppedLetter = document.getElementById(data);
 
-	// Obtener el elemento donde se soltó la letra
-	const space = e.target;
-    
+    // Obtener el elemento donde se soltó la letra
+    const space = e.target;
+
     if (space.getAttribute("class") === "answer-space") {
         // Mover la letra al espacio de respuesta correspondiente
         droppedLetter.parentNode.removeChild(droppedLetter);
-	    space.appendChild(droppedLetter);
+        space.appendChild(droppedLetter);
     } else if (droppedLetter.parentNode !== lettersDiv && (space.getAttribute("class") !== "letter" || space.parentNode === lettersDiv)) {
         // Quitar la letra y devolverla 
         droppedLetter.parentNode.removeChild(droppedLetter);
-	    lettersDiv.appendChild(droppedLetter);
+        lettersDiv.appendChild(droppedLetter);
     } else return;
 
     // Según el número de letras colocadas, se activa o no el botón
     let n = 0;
     for (let i = 0; i < answerDiv.childElementCount; i++)
-        if(answerDiv.children[i].childElementCount === 1) ++n;
+        if (answerDiv.children[i].childElementCount === 1) ++n;
     verifyButton.toggleAttribute("disabled", n !== color.length);
 });
 
 // Verificar si el nombre del color es correcto
-verifyButton.addEventListener("click", function(){
+verifyButton.addEventListener("click", function () {
     let word = "";
     const spaces = document.querySelectorAll(".answer-space");
-    spaces.forEach(function(value) {
+    spaces.forEach(function (value) {
         word += value.firstChild.innerHTML;
     });
 
@@ -108,7 +108,7 @@ verifyButton.addEventListener("click", function(){
         alert("¡Correcto! :D");
         startGame();
         // Quizá añadir un botón intermedio para darle control al usuario
-    } else { 
+    } else {
         answerDiv.style.backgroundColor = "red";
         alert("La respuesta no es correcta :(" +
             "\nHas escrito: \"" + word.toUpperCase() + "\" y debía ser \"" + color.toUpperCase() + "\"");
