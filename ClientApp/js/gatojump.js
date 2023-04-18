5f8fa3193d7c0bc355449013f691083d051b614a
xmariafdz <115629455+xmariafdz@users.noreply.github.com>
2023-04-18 21:57:13 +0200

    commit
    commin

--- ClientApp/js/gatojump.js
+++ ClientApp/js/gatojump.js
@@ -11,6 +11,19 @@ eventImages.forEach(image => {
   });
 });
 
+const gatete = document.getElementById("gatete");
+const boton = document.querySelectorAll(".btn");
+
+boton.forEach(image => {
+  image.addEventListener("mouseenter", () => {
+    gatete.classList.add("salta");
+  });
+
+  image.addEventListener("mouseleave", () => {
+    gatete.classList.remove("salta");
+  });
+});
+
 const botonAnimales = document.querySelectorAll(".button2");
 const bubbleAnimales = document.getElementById("bubbleAnimales");
 
@@ -57,5 +70,112 @@ botonColores.forEach(boton => {
     bubbleBienvenida.style.display = 'block';
   });
 });
+
+const gatitoMenu = document.getElementById("gatitoMenu");
+const botonMenu = document.querySelectorAll(".card");
+
+
+botonMenu.forEach(image => {
+  image.addEventListener("mouseenter", () => {
+    gatitoMenu.classList.add("salta2");
+  });
+
+  image.addEventListener("mouseleave", () => {
+    gatitoMenu.classList.remove("salta2");
+  });
+});
+
+const bubbleMenu = document.getElementById("bubbleMenu");
+const botonPractica = document.querySelectorAll(".botonPractica");
+const bubbleModoPractica = document.getElementById("bubblePractica");
+
+botonPractica.forEach(boton => {
+  boton.addEventListener('mouseover', () => {
+    bubbleModoPractica.style.display = 'block';
+    bubbleMenu.style.display = 'none';
+  });
+
+  boton.addEventListener('mouseout', () => {
+    bubbleModoPractica.style.display = 'none';
+    bubbleMenu.style.display = 'block';
+  });
+});
+
+const botonEstudio = document.querySelectorAll(".botonEstudio");
+const bubbleModoEstudio = document.getElementById("bubbleEstudio");
+
+botonEstudio.forEach(boton => {
+  boton.addEventListener('mouseover', () => {
+    bubbleModoEstudio.style.display = 'block';
+    bubbleMenu.style.display = 'none';
+  });
+
+  boton.addEventListener('mouseout', () => {
+    bubbleModoEstudio.style.display = 'none';
+    bubbleMenu.style.display = 'block';
+  });
+});
+
+const botonTienda = document.querySelectorAll(".botonTienda");
+const bubbleTienda = document.getElementById("bubbleTienda");
+
+botonTienda.forEach(boton => {
+  boton.addEventListener('mouseover', () => {
+    bubbleTienda.style.display = 'block';
+    bubbleMenu.style.display = 'none';
+  });
+
+  boton.addEventListener('mouseout', () => {
+    bubbleTienda.style.display = 'none';
+    bubbleMenu.style.display = 'block';
+  });
+});
+
+const bubbleWelc = document.getElementById("bubbleWelcomeStudy");
+const botonCol = document.querySelectorAll(".botonCol");
+const bubbleColoresStudy = document.getElementById("bubbleColoresStudy");
+
+botonCol.forEach(boton => {
+  boton.addEventListener('mouseover', () => {
+    bubbleColoresStudy.style.display = 'block';
+    bubbleWelc.style.display = 'none';
+  });
+
+  boton.addEventListener('mouseout', () => {
+    bubbleColoresStudy.style.display = 'none';
+    bubbleWelc.style.display = 'block';
+  });
+});
+
+const botonAni = document.querySelectorAll(".botonAni");
+const bubbleAnimalesStudy = document.getElementById("bubbleAnimalesStudy");
+
+botonAni.forEach(boton => {
+  boton.addEventListener('mouseover', () => {
+    bubbleAnimalesStudy.style.display = 'block';
+    bubbleWelc.style.display = 'none';
+  });
+
+  boton.addEventListener('mouseout', () => {
+    bubbleAnimalesStudy.style.display = 'none';
+    bubbleWelc.style.display = 'block';
+  });
+});
+
+const botonNume = document.querySelectorAll(".botonNum");
+const bubbleNumerosStudy = document.getElementById("bubbleNumerosStudy");
+
+botonNume.forEach(boton => {
+  boton.addEventListener('mouseover', () => {
+    bubbleNumerosStudy.style.display = 'block';
+    bubbleWelc.style.display = 'none';
+  });
+
+  boton.addEventListener('mouseout', () => {
+    bubbleNumerosStudy.style.display = 'none';
+    bubbleWelc.style.display = 'block';
+  });
+});
+
 
 
