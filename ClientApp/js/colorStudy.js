41fe5a0202b303355f6e90423a92e78021a4bf62
Nu36 <nurirt36@uma.es>
2023-04-30 19:15:46 +0200

    Quitado un botón innecesario que yo misma había puesto pa hacer lo de los sonidos

--- ClientApp/js/colorStudy.js
+++ ClientApp/js/colorStudy.js
@@ -17,27 +17,6 @@ const colors2 = [
 ]
 
 const botonpamostrar = document.getElementsByClassName("paramostrarcolors")[0];
-const botonaudios = document.getElementById("audios");
-const verde = document.getElementsByClassName("green")[0];
-const rojo = document.getElementsByClassName("red")[0];
-const azul = document.getElementsByClassName("blue")[0];
-const amarillo = document.getElementsByClassName("yellow")[0];
-const blanco = document.getElementsByClassName("white")[0];
-const morado = document.getElementsByClassName("purple")[0];
-const aqua = document.getElementsByClassName("aqua")[0];
-const negro = document.getElementsByClassName("black")[0];
-const naranja = document.getElementsByClassName("orange")[0];
-const gris = document.getElementsByClassName("gray")[0];
-const audiogreen = document.getElementById('audiogreen');
-const audiored = document.getElementById('audiored');
-const audioblue = document.getElementById('audioblue');
-const audioyellow = document.getElementById('audioyellow');
-const audiowhite = document.getElementById('audiowhite');
-const audiopurple = document.getElementById('audiopurple');
-const audioaqua = document.getElementById('audioaqua');
-const audioblack = document.getElementById('audioblack');
-const audioorange = document.getElementById('audioorange');
-const audiogray = document.getElementById('audiogray');
 
 function mostrarContenidoColor() {
     document.getElementById("contenidocolors").style.display = "block";
@@ -46,50 +25,6 @@ function mostrarContenidoColor() {
 
 function mostrarBoton() {
     botonpamostrar.style.display = "block";
-}
-
-
-button.onclick = function () {
-    //console.log(colorDisplays);
-    let colors = colors2;
-    for (let i = 0; i < colorDivs.length; i++) {
-        const color = colorDivs.item(i);
-        if (!color) {
-            console.log("ERROR");
-            continue;
-        }
-        if (i == 0 && color.getAttribute("class") !== "green")
-            colors = colors1;
-
-        const colorName = colors[i];
-        color.setAttribute("class", colorName[1].toLowerCase());
-        const colorDisplay = color.firstElementChild;
-
-        if (!colorDisplay) {
-            console.log("ERROR");
-            continue;
-        }
-
-        colorDisplay.innerHTML = colorName[0] + "<br/>" + colorName[1] + "<br/>";
-        
-        let audioFileName = "resources/sounds/" + colorName[0].toLowerCase() + " " + colorName[1].toLowerCase() + ".aac";
-            
-        let audio = new Audio(audioFileName);
-        color.audio = audio;
-    
-        color.addEventListener("mouseover", function () {
-            console.log(color.audio)
-            color.audio.play();
-        });
-    
-        color.addEventListener("mouseout", function(){
-            color.audio.pause();
-            color.audio.currentTime = 0;
-        });
-    }
-}
-
-botonaudios.onclick = function(){
     let colors = colors1;
     for (let i = 0; i < colorDivs.length; i++) {
         const color = colorDivs.item(i);
@@ -117,7 +52,48 @@ botonaudios.onclick = function(){
             color.audio.currentTime = 0;
         });
     }
-};
+}
+
+
+button.onclick = function () {
+    //console.log(colorDisplays);
+    let colors = colors2;
+    for (let i = 0; i < colorDivs.length; i++) {
+        const color = colorDivs.item(i);
+        if (!color) {
+            console.log("ERROR");
+            continue;
+        }
+        if (i == 0 && color.getAttribute("class") !== "green")
+            colors = colors1;
+
+        const colorName = colors[i];
+        color.setAttribute("class", colorName[1].toLowerCase());
+        const colorDisplay = color.firstElementChild;
+
+        if (!colorDisplay) {
+            console.log("ERROR");
+            continue;
+        }
+
+        colorDisplay.innerHTML = colorName[0] + "<br/>" + colorName[1] + "<br/>";
+
+        let audioFileName = "resources/sounds/" + colorName[0].toLowerCase() + " " + colorName[1].toLowerCase() + ".aac";
+            
+        let audio = new Audio(audioFileName);
+        color.audio = audio;
+    
+        color.addEventListener("mouseover", function () {
+            console.log(color.audio)
+            color.audio.play();
+        });
+    
+        color.addEventListener("mouseout", function(){
+            color.audio.pause();
+            color.audio.currentTime = 0;
+        });
+    }
+}
 
 
 // Ejecutamos la función al cargar la página
